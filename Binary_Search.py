24eb9d5f78bc75a6af92a8a8cd6898b614bff5c4
yeesunch <yeesun.chu@hotmail.com>
2021-03-09 19:11:17 -0800

    Add files via upload

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,1 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  9 19:10:04 2021
 
+@author: yeesun
+
+Invariant: If the target value X is present in the array, then the target value is present in the current range
+
+"""
+
+# Implementation: A standard Binary Search algo has two implementations:
